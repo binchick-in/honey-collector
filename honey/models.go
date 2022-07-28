19f6266108aa7df5bf95ed851ce44a3c156d9e59
binchick.in <git@binchick.in>
2022-07-27 19:32:29 -0700

    some tweaks and shit

--- honey/models.go
+++ honey/models.go
@@ -1,44 +1,47 @@
 package honey
 
 import (
-	"strings"
-	"net/http"
-	"encoding/json"
+    "time"
+    "strings"
+    "net/http"
+    "encoding/json"
 )
 
 type LoggedRequest struct {
-    Host string `json:"host"`
-    Method string `json:"method"`
-	Path string `json:"path"`
-    RemoteAddress string `json:"remote_address"`
-    UserAgent string `json:"user_agent"`
+    Host string                   `json:"host"`
+    Method string                 `json:"method"`
+    Path string                   `json:"path"`
+    RemoteAddress string          `json:"remote_address"`
+    UserAgent string              `json:"user_agent"`
     QueryParams map[string]string `json:"query_params"`
-    Headers map[string]string `json:"headers"`
-    Body string `json:"body"`
+    Headers map[string]string     `json:"headers"`
+    Body string                   `json:"body"`
+    Time uint64                   `json:"time"`
 }
 
 func (lr *LoggedRequest) ToJson() string {
-	j, err := json.Marshal(lr)
-	if err != nil {
-		panic(err)
-	}
-	return string(j)
+    j, err := json.Marshal(lr)
+    if err != nil {
+        panic(err)
+    }
+    return string(j)
 }
 
 func NewLoggedRequest(r http.Request) LoggedRequest {
-	var decodedBody string
-	if r.Method == http.MethodPost {
-		decodedBody = decodeRequestBody(r.Body)
-	}
-	remoteHost := strings.Split(r.RemoteAddr, ":")
-	return LoggedRequest{
+    var decodedBody string
+    if r.Method == http.MethodPost {
+        decodedBody = decodeRequestBody(r.Body)
+    }
+    remoteHost := strings.Split(r.RemoteAddr, ":")
+    return LoggedRequest{
         Host:          r.Host,
         Method:        r.Method,
-		Path:          r.URL.Path,
+        Path:          r.URL.Path,
         RemoteAddress: remoteHost[0],
         UserAgent:     r.UserAgent(),
         QueryParams:   processMapOfSlices(r.URL.Query()),
         Headers:       processMapOfSlices(r.Header),
         Body:          decodedBody,
+        Time:          uint64(time.Now().Unix()),
     }
 }
--- main.go
+++ main.go
@@ -1,27 +1,27 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
+    "fmt"
+    "net/http"
 
-	"honey-collector/honey"
+    "honey-collector/honey"
 )
 
 var honeyClient honey.HoneyClient
 
 func ReqHandler(resp http.ResponseWriter, req *http.Request) {
-	lr := honey.NewLoggedRequest(*req)
-	fmt.Println(lr.ToJson())
-	fmt.Fprintf(resp, "ree")
+    lr := honey.NewLoggedRequest(*req)
+    fmt.Println(lr.ToJson())
+    fmt.Fprintf(resp, "ree")
 }
 
 func main() {
-	defaultPort := 8081
-	honeyClient = honey.NewHoneyClientFromEnv()
-	fmt.Printf("Starting honey pot on port: %d\n", defaultPort)
-	http.HandleFunc("/", ReqHandler)
-	serverStartErr := http.ListenAndServe(fmt.Sprintf(":%d", defaultPort), nil)
-	if serverStartErr != nil {
-		panic(serverStartErr)
-	}
+    defaultPort := 8081
+    honeyClient = honey.NewHoneyClientFromEnv()
+    fmt.Printf("Starting honey pot on port: %d\n", defaultPort)
+    http.HandleFunc("/", ReqHandler)
+    serverStartErr := http.ListenAndServe(fmt.Sprintf(":%d", defaultPort), nil)
+    if serverStartErr != nil {
+        panic(serverStartErr)
+    }
 }
